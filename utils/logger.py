8a4016c42ca536150745a80c12dbf6e91a8549c2
zhuzhaorui <114720655+zhuzhaorui@users.noreply.github.com>
2026-05-12 00:56:54 +0800

    Create logger.py

---
+++ utils/logger.py
@@ -0,0 +1,10 @@
+import logging
+
+# 配置日志格式
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
+
+logger = logging.getLogger(__name__)
